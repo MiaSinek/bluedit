10552ae507e0471b6f7b6b2572de6e4ec06ae3fe
Mia Sinek <mia@miasinek.com>
2019-11-05 14:12:30 +0100

    Set up main nav dropdows to work properly

--- app/javascript/components/dropdown.js
+++ app/javascript/components/dropdown.js
@@ -1,18 +1,46 @@
-// TODO: SVG icons should be white on hover
-
+// app/javascript/components/dropdown.js
 document.addEventListener("turbolinks:load", () => {
-  const dropdown = document.querySelector(".js-dropdown-user-target");
-  const dropdownList = document.querySelector(".dropdown-list");
 
-  dropdown.addEventListener("click", (evt) => evt.preventDefault());
+  function dropdown() {
+    const dropDownLinks = document.querySelectorAll('.js-nav .dropdown a.dropdown-target');
 
-  document.addEventListener("click", (evt) => {
-    if(evt.target.closest(".dropdown")) {
-      // show dropdown if dropdown is clicked
-      dropdownList.classList.remove("hidden");
-    } else {
-      // hide dropdown if outside of dropdown is clicked
-      dropdownList.classList.add("hidden");
+    dropDownLinks.forEach(function(dropdown) {
+      dropdown.addEventListener('click', function(event) {
+
+        const dropdownTarget = event.currentTarget.nextElementSibling;
+
+        // toggle dropdown
+        dropdownTarget.classList.toggle('hidden');
+        // hide dropdown when clicking outside the dropdown area
+        hideOnClickOutside(event.currentTarget);
+        event.preventDefault();
+      });
+    });
+  }
+
+  // TODO Refactor this - it is a mess!
+  // We should attach the event outsideClickListener if any of the dropdowns are open
+  // otherwise (when the dropdowns have been closed) remove it
+  function hideOnClickOutside(element) {
+    const outsideClickListener = function(event) {
+      if (!element.contains(event.target)) {
+        if (!!element && !!(element.offsetWidth || element.offsetHeight || element.getClientRects().length)) {
+          element.nextElementSibling.classList.toggle('hidden');
+          removeClickListener();
+        }
+      }
     }
-  });
+
+    const removeClickListener = function() {
+      document.removeEventListener('click', outsideClickListener);
+    }
+
+    document.addEventListener('click', outsideClickListener);
+  }
+
+function init() {
+  dropdown();
+}
+
+init();
 });
