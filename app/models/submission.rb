11c0484c1fc87c7cbdbbd3a8088dc6b5bbf61bf9
Mia Sinek <mia@miasinek.com>
2019-11-11 18:35:10 +0100

    Validate that only video or image is being uploaded, but not both

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -8,9 +8,19 @@ class Submission < ApplicationRecord
   belongs_to :community
   has_many :comments, dependent: :destroy
 
+  validate :image_or_video, if: -> { submission_image.present? || submission_video.present? }
+
   validates :title, presence: true
   validates :body, length: { maximum: 8000 }
   validates :url, url: { allow_blank: true }
 
   acts_as_votable
+
+  private
+
+  def image_or_video
+    if submission_image.present? && submission_video.present?
+      errors.add(:base, "Add an image or a video, not both")
+    end
+  end
 end
