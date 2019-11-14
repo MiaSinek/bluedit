383d07c01d1c0f56cf48fa6fa3b5507cd6c00aa9
Mia Sinek <mia@miasinek.com>
2019-11-14 14:35:13 +0100

    Requiring Action Text in application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,3 +20,6 @@ import "components/modal";
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+require("trix")
+require("@rails/actiontext")
