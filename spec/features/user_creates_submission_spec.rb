f371498d155847c9528fcd97a0596a24fa5e9fcd
Mia Sinek <mia@miasinek.com>
2019-11-21 20:27:40 +0100

    Creating scenarios for creating image and link submissions

--- spec/features/user_creates_submission_spec.rb
+++ spec/features/user_creates_submission_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
-feature "user creates submission", js: true do
-  scenario "of type 'post' successfully" do
+feature "user creates submission" do
+  scenario "of type 'post' successfully", js:true do
     user = User.create(username: "Someone", email: "someone@example.com", password: "password", password_confirmation: "password")
 
     community = Community.create(name: 'testcomm1', title: 'Test Community 1', user_id: user.id)
@@ -22,5 +22,52 @@ feature "user creates submission", js: true do
     expect(page).to have_css('h1', text:"w/#{community.name}")
     expect(page).to have_css('h1', text: 'Test Submission')
     expect(page).to have_css('div.trix-content', text: 'Test Submission Body!')
+  end
+
+  scenario "of type 'link' successfully" do
+    user = User.create(username: "Someone", email: "someone@example.com", password: "password", password_confirmation: "password")
+
+    community = Community.create(name: 'testcomm1', title: 'Test Community 1', user_id: user.id)
+
+    sign_in user.email, user.password
+
+    click_on "New Submission"
+
+    find(:xpath, '//a[contains(., "Link")]').click
+
+    fill_in 'submission[title]', with: 'Test Link Submission'
+    fill_in 'submission[url]', with: 'https://www.google.com'
+
+    select community.name, from: 'submission[community_id]'
+
+    click_on "Create Submission"
+
+    expect(page).to have_css('h1', text:"w/#{community.name}")
+    expect(page).to have_css('h1', text: 'Test Link Submission')
+    expect(page).to have_link('https://www.google.com', href:'https://www.google.com')
+  end
+
+  scenario "of type 'image' successfully" do
+    user = User.create(username: "Someone", email: "someone@example.com", password: "password", password_confirmation: "password")
+
+    community = Community.create(name: 'testcomm1', title: 'Test Community 1', user_id: user.id)
+
+    sign_in user.email, user.password
+
+    click_on "New Submission"
+
+    find(:xpath, '//a[contains(., "Image")]').click
+
+    fill_in 'submission[title]', with: 'Test Link Submission'
+    attach_file("Upload Image", Rails.root + "spec/fixtures/images/image1.jpeg")
+
+
+    select community.name, from: 'submission[community_id]'
+
+    click_on "Create Submission"
+
+    expect(page).to have_css('h1', text:"w/#{community.name}")
+    expect(page).to have_css('h1', text: 'Test Link Submission')
+    expect(page).to have_css("img[src*='image1.jpeg']")
   end
 end
