9601a3ffb3588d80dc450a5a0bd1a580fd11d928
Mia Sinek <mia@miasinek.com>
2019-11-26 11:26:54 +0100

    Refactor flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,27 @@ module ApplicationHelper
     inline_svg(filename, aria: true, nocomment: true, title: title, class: styles)
   end
 
-  #Multi line comment
+  def alert_class_for(type)
+    if type == "alert"
+      "bg-red-500"
+    elsif type == "notice"
+      "bg-green-500"
+    end
+  end
+
+  #Multi-line comment
   def comment
   end
 end
+
+
+# <% if type == "alert" %>
+#   <div class="bg-red-500">
+#     <div class="container px-2 mx-auto py-4 text-white text-center font-medium font-sans"><%= message %></div>
+#   </div>
+# <% end %>
+# <% if type == "notice" %>
+#   <div class="bg-green-500">
+#     <div class="container px-2 mx-auto py-4 text-white text-center font-medium font-sans"><%= message %></div>
+#   </div>
+# <% end %>
