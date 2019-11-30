b8b027cdb6b5a5acfb900604ecb66e0d384b0689
Mia Sinek <mia@miasinek.com>
2019-11-30 11:12:06 +0100

    Create community feature

--- spec/factories/communities.rb
+++ spec/factories/communities.rb
@@ -1,7 +1,25 @@
 FactoryBot.define do
+  # community factory without associated submissions
   factory :community do
-    name { "Test community name" }
-    title { "Test community title" }
-    user { User.first || association(:user) }
+    sequence(:name) { |n| "Test community#{n} name" }
+    sequence(:title) { |n| "Test community#{n} title" }
+    sequence(:user) { |n| User.all[n] || association(:user) }
+
+    #  create submission data after the community has been created
+    factory :community_with_submissions do
+      # submission_count is declared as a transient attribute and available in
+      # attributes on the factory, as well as the callback via the evaluator
+      transient do
+        submissions_count { 5 }
+      end
+
+      # the after(:create) yields two values; the community instance itself and the
+      # evaluator, which stores all values from the factory, including transient
+      # attributes; `create_list`'s second argument is the number of records
+      # to create and we make sure the user is associated properly to the post
+      after(:create) do |community, evaluator|
+        create_list(:submission, evaluator.submissions_count, community: community)
+      end
+    end
   end
  end
---
+++ spec/support/features/create_community.rb
@@ -0,0 +1,12 @@
+module Features
+  def create_community
+    click_on "New Community"
+
+    fill_in "community[name]", with: "rubyonrails"
+    fill_in "community[title]", with: "Ruby on Rails"
+    fill_in "community[sidebar]", with: "Ruby on Rails Palooza"
+    fill_in "community[description]", with: "Best Ruby on Rails stuff"
+
+    click_on "Create Community"
+  end
+end
