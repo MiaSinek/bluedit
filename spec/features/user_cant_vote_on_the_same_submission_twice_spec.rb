c1220a73a63aebe3201b28a26704bb928227b52f
Mia Sinek <mia@miasinek.com>
2019-12-03 18:18:18 +0100

    Add user cant vote on the same submission twice spec

---
+++ spec/features/user_cant_vote_on_the_same_submission_twice_spec.rb
@@ -0,0 +1,23 @@
+module Features; end
+require "rails_helper"
+
+
+feature "User votes on submission twice" do
+  scenario "unsuccessfully", js: true do
+    community = create(:community, :with_submissions, submissions_count: 1)
+    submission = community.submissions.last
+    user = create(:user)
+
+    login_as(user, :scope => :user)
+
+    visit submission_path(submission)
+
+    upvote_link = find(:css, "a[href='#{upvote_submission_path(submission)}']")
+
+    upvote_link.click
+    upvote_link.click
+
+    expect(page).to have_css("div#submission-#{submission.id} div.submission-voting span", text: 1)
+    expect(page).to have_selector("#voting-notice", text: "You already voted for this submission")
+  end
+end
