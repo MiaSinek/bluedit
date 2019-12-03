cdd3cf00b7c6dc0656ef220f80d6366936325723
Mia Sinek <mia@miasinek.com>
2019-12-03 11:23:03 +0100

    Add user can edit own submission spec

--- spec/features/user_can_edit_own_submission_only_spec.rb
+++ spec/features/user_can_edit_own_submission_only_spec.rb
@@ -1,7 +1,7 @@
 require 'support/features/sign_in.rb'
 require "rails_helper"
 
-feature "user edits own feature" do
+feature "user edits own submission" do
   scenario "successfully" do
     community1 = create(:community)
     community2 = create(:community, :with_submissions, submissions_count: 1)
@@ -18,5 +18,34 @@ feature "user edits own feature" do
 
     expect(page).to have_css "div.submission-content h1.text-xl", text: "Edited Submission Title"
     expect(page).to have_css "h1", text: community2.name
+  end
+end
+
+feature "There is an 'Edit' link" do
+  before(:each) do
+    @community = create(:community, :with_submissions, submissions_count: 1)
+    @submission = @community.submissions.last
+
+    login_as(@submission.user, :scope => :user)
+  end
+
+  scenario "on the user's own submission's show page" do
+    visit submission_path(@submission)
+
+    expect(page).to have_xpath "//div[@class='submission-content']//a[contains(@href,'/edit')]"
+  end
+
+  scenario "on the root path to the user's own submission" do
+    create(:subscription, community: @community, user: @submission.user)
+
+    visit root_path
+
+    expect(page).to have_xpath "//a[contains(@href,'#{@submission.id}/edit')]"
+  end
+
+  scenario "on the community's index page to the user's own submission" do
+    visit community_path(@community)
+
+    expect(page).to have_xpath "//a[contains(@href,'#{@submission.id}/edit')]"
   end
 end
