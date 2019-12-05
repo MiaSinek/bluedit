f9efc3f7eb7d741ac325cbe02786e99bb3bdcf45
Mia Sinek <mia@miasinek.com>
2019-12-05 18:42:32 +0100

    Add support function - can click on element identified with CSS; Also, add second optional parameter - path

--- spec/support/features/guest_redirected_to_login.rb
+++ spec/support/features/guest_redirected_to_login.rb
@@ -1,7 +1,15 @@
 module Features
-  def guest_is_redirected_to_login_when_clicking_on(link_text)
-    visit root_path
+  def guest_is_redirected_to_login_when_clicking_on_text(link_text, path=root_path)
+    visit path
     click_on link_text
+
+    expect(page).to have_current_path(new_user_session_path)
+    expect(page).to have_text "You need to log in or sign up before continuing."
+  end
+
+  def guest_is_redirected_to_login_when_clicking_on_css_element(css_query, path=root_path)
+    visit path
+    find(:css, css_query).click
 
     expect(page).to have_current_path(new_user_session_path)
     expect(page).to have_text "You need to log in or sign up before continuing."
