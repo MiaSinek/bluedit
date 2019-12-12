0c1709b4269584e56591efccdbb479ab28fc3141
Mia Sinek <mia@miasinek.com>
2019-12-12 21:12:36 +0100

    Spec: User edits own submission, tries to store it in invalid state - unsuccessfully

--- spec/features/user/user_can_edit_own_comments_spec.rb
+++ spec/features/user/user_can_edit_own_comments_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
-feature "user edits own comment" do
-  scenario "successfully on the submission show page", js:true do
+feature "user edits own comment on the submission show page" do
+  scenario "successfully", js:true do
     submission = create(:submission, :with_body, :with_comments)
     comment = submission.comments.sample
     user = comment.user
@@ -15,5 +15,21 @@ feature "user edits own comment" do
 
     expect(page).to have_css "div#comment-#{comment.id}", text: "Edited Comment Body"
     expect(page).to have_text "Comment was successfully updated."
+  end
+
+  scenario "unsuccessfully", js:true do
+    submission = create(:submission, :with_body, :with_comments)
+    comment = submission.comments.sample
+    user = comment.user
+
+    login_as user
+    visit submission_path(submission)
+
+    find(:css, "a[href='#{edit_submission_comment_path(submission, comment)}']").click
+    find(:xpath, "//form[@class='edit_comment']//textarea").set("")
+    click_on 'Update Comment'
+
+    expect(page).to have_text("Comment could not be updated. Please add some awesomesauce!")
+    expect(page).to have_current_path(submission_path(submission))
   end
 end
