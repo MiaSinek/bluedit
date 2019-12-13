eaf698a00aaca8ab6a7392788ce05896ed6fef3f
Mia Sinek <mia@miasinek.com>
2019-12-13 20:24:40 +0100

    Add dependent:destroy on associations that should be destroyed when community is destroyed

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,8 +1,8 @@
 class Community < ApplicationRecord
   belongs_to :user
 
-  has_many :submissions
-  has_many :subscriptions
+  has_many :submissions, dependent: :destroy
+  has_many :subscriptions, dependent: :destroy
   has_many :users, through: :subscriptions
 
   validates_presence_of :title, :name
