e3a2d9437a8a2c3ff74816f6bdc26c0480fb7afa
Mia Sinek <mia@miasinek.com>
2019-12-18 10:46:56 +0100

    Standardize communities_controller

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,51 +1,76 @@
 class CommunitiesController < ApplicationController
-  before_action :set_community, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:show, :index]
 
   def index
-    @communities = Community.all
+    load_communities
   end
 
   def show
+    load_community
   end
 
   def new
-    @community = current_user.communities_authored.new
-  end
-
-  def edit
+    build_community
   end
 
   def create
-    @community = current_user.communities_authored.new(community_params)
-
-    if CommunityCreator.new(@community, current_user).save
-      redirect_to @community, notice: 'Community was successfully created.'
-    else
-      render :new
-    end
+    build_community
+    save_community('Community was successfully created.') or render :new
   end
 
+  def edit
+    load_community
+    build_community
+  end
+
+
   def update
-    if @community.update(community_params)
-      redirect_to @community, notice: 'Community was successfully updated.'
-    else
-      render :edit
-    end
+    load_community
+    build_community
+    save_community('Community was successfully updated.') or render :edit
   end
 
   def destroy
+    load_community
     @community.destroy
 
-    redirect_to communities_url, notice: 'Community was successfully destroyed.'
+    redirect_to communities_path, notice: 'Community was successfully destroyed.'
   end
 
   private
-    def set_community
-      @community = Community.find(params[:id])
+    def load_communities
+      @communities ||= community_scope.to_a
+    end
+
+    def load_community
+      @community ||=  community_scope.find(params[:id])
+    end
+
+    def build_community
+      @community ||= community_scope.build
+      @community.attributes = community_params
+    end
+
+    def save_community(notice)
+      if CommunityCreator.new(@community, current_user).save
+        redirect_to @community, notice: notice
+      end
+    end
+
+    def community_scope
+      if creating_community?
+        current_user.communities_authored
+      else
+        Community.all
+      end
     end
 
     def community_params
-      params.require(:community).permit(:name, :title, :description, :sidebar)
+      community_params = params[:community]
+      community_params ? community_params.permit(:name, :title, :description, :sidebar) : {}
     end
-end
+
+    def creating_community?
+      user_signed_in? && params[:action] == 'create'
+    end
+ end
